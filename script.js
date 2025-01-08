6d1acb39fc65913020e0d325f59bd32807de05a4
erikalaiane <>
2025-01-08 12:39:44 -0300

    header completo

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Função para alternar o menu
 function toggleMenu() {
   const menu = document.querySelector('.menu'); // Seleciona o menu
-  menu.classList.toggle('active'); // Adiciona ou remove a classe 'active'
+  menu.classList.toggle('active'); // Alterna a classe 'active'
 }
